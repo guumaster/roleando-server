456b85fa8ce5cf8891e2b2a7b893eda290bdc2d0
guumaster <guuweb@gmail.com>
2017-04-17 21:40:50 +0200

    latest client version. try to fix error logger.

--- src/server.js
+++ src/server.js
@@ -2,10 +2,12 @@
 
 const express = require('express')
 const bodyParser = require('body-parser')
-const morgan = require('morgan')
+// const morgan = require('morgan')
 const config = require('config')
 
-const errorHandler = require('./middlewares/error_handler')
+const errors = require('restify-errors')
+
+// const errorHandler = require('./middlewares/error_handler')
 const routes = require('./routes')
 const statics = require('./routes/statics')
 
@@ -16,8 +18,21 @@ const app = express({
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
 
-app.use(errorHandler)
-//app.use(morgan('combined'))
+// app.use(errorHandler)
+// app.use(morgan('combined'))
+app.use(function (err, req, res, next) {
+  console.log(err, err.message, err.stack)
+
+  if (req.header('content-type') === 'application/json') {
+    if (err instanceof errors.HttpError) {
+      res.status(err.statusCode).send(err.body)
+      return
+    }
+    res.status(500).send(new errors.InternalServerError(err).body)
+    return
+  }
+  res.status(500).send(err.message || 'Something broke!')
+})
 
 app.use(routes)
 app.use(statics)
