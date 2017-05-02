d8630f0900799eafd083c02535c8ac0558de0d14
guumaster <guuweb@gmail.com>
2017-05-03 00:36:45 +0200

    improved with history

--- pages/rolodromo.vue
+++ pages/rolodromo.vue
@@ -13,7 +13,8 @@
             persistent-hint
             v-model="rawText"
             @input="update"
-            class="input-group--focused"
+            class="input-group--focused big-line"
+            prepend-icon="forward"
           ></v-text-field>
           <v-spacer></v-spacer>
           <v-btn class="red roller-btn" floating large @click.native="run" :disabled="disabled">
@@ -24,21 +25,34 @@
       <v-card-row>
         <v-card-text>
           <span class="display-1 grey--text">{{roll.taken || roll.rolls}} {{mod}} = </span>
-          <span class="red--text display-1">
-             <animate-number
-               v-if="clientOnly"
-               ref="anim"
-               :from="roll.total - 5"
-               :to="roll.total"
-               mode="manual"
-               duration="500"
-               easing="swingTo"
-               from-color="#fff"
-             ></animate-number>
+          <span class="red--text display-2">
+                  <animate-number
+                    v-if="clientOnly"
+                    ref="anim"
+                    :from="roll.total - 5"
+                    :to="roll.total"
+                    mode="manual"
+                    duration="500"
+                    easing="swingTo"
+                    from-color="#fff"
+                  ></animate-number>
           </span>
         </v-card-text>
       </v-card-row>
     </v-card>
+    <v-list subheader class="white">
+      <v-subheader>Previas</v-subheader>
+      <v-list-item v-for="(item,i) in past" :key="i">
+        <v-list-tile avatar>
+          <v-list-tile-content>
+            <v-list-tile-title>{{item.rolls}}</v-list-tile-title>
+          </v-list-tile-content>
+          <v-list-tile-avatar>
+            <v-btn outline icon class="red--text">{{item.total}}</v-btn>
+          </v-list-tile-avatar>
+        </v-list-tile>
+      </v-list-item>
+    </v-list>
   </div>
 </template>
 <script>
@@ -52,7 +66,8 @@
         disabled: false,
         rawText: 'mejores 3 de 4d6+1',
         roll: 0,
-        roller: null
+        roller: null,
+        past: []
       }
     },
     watch: {
@@ -71,11 +86,15 @@
     },
     methods: {
       logger (r) {
+        // TODO: a nice logger
       },
       run () {
+        if (this.roll) {
+          this.past.unshift(this.roll)
+          this.past.splice(5)
+        }
         this.roll = this.roller(true)
         this.from = this.roll.total - 5
-
       },
       update: debounce(function (value) {
         try {
@@ -89,6 +108,7 @@
     },
     created () {
       this.roller = rpgen.roller.create(this.rawText, {fn: this.logger})
+      this.past = []
       if (this.clientOnly) {
         this.run()
       }
@@ -96,6 +116,11 @@
   }
 </script>
 <style scoped>
+  .big-line input {
+    line-height: 15em;
+    height: 5em !important;
+  }
+
   .roll-btn {
     width: 30%;
   }
